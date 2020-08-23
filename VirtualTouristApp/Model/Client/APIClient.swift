13df52e447cba9c7fd4de4e3606a5f1510346456
Khalid Aqeeli <aqeelikh@gmail.com>
2020-08-23 14:29:15 +0300

    refactor: Remove unused code from APIClient.swift
    APICinet class should only have relevant code to its project, the code removed was used in another project ( OnTheMap Project),  that have different requirement from the VirtualTourist project.

--- VirtualTouristApp/Model/Client/APIClient.swift
+++ VirtualTouristApp/Model/Client/APIClient.swift
@@ -13,23 +13,16 @@ class APIClient {
     
     
     enum Endpoint {
-        static let base:String = "https://onthemap-api.udacity.com/v1/"
-        static let downloadLimit:String = "StudentLocation?order=-updatedAt&limit=100"
-        static let loginPath:String = "session"
-        static let studentLocation:String = "StudentLocation"
+        static let base:String = ""
+        static let loginPath:String = ""
         
         case login
-        case getStudentLocation
-        case postStudentLocation
+
         
         var stringValue: String {
             switch self {
             case .login:
                 return Endpoint.base + Endpoint.loginPath
-            case .getStudentLocation:
-                return Endpoint.base + Endpoint.downloadLimit
-            case .postStudentLocation:
-                return Endpoint.base + Endpoint.studentLocation
             }
         }
         var url:URL {
